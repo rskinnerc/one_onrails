5e1f204f92cdd8cb7f1cc237befda4237516f5f5
Ronald Skinner <rjskinnerc@unal.edu.co>
2024-11-17 06:53:25 -0500

    Remoce comments on profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,21 +1,16 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: %i[ show edit update destroy ]
 
-
-  # GET /profile
   def show
   end
 
-  # GET /profiles/new
   def new
     @profile = current_user.build_profile
   end
 
-  # GET /profiles/1/edit
   def edit
   end
 
-  # POST /profiles
   def create
     @profile = current_user.build_profile(profile_params)
 
@@ -26,7 +21,6 @@ class ProfilesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /profiles/1
   def update
     if profile_params[:remove_avatar] == "1"
       @profile.avatar.purge
@@ -39,19 +33,16 @@ class ProfilesController < ApplicationController
     end
   end
 
-  # DELETE /profiles/1
   def destroy
     @profile.destroy!
     redirect_to profile_path, notice: "Profile was successfully destroyed.", status: :see_other
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_profile
       @profile = current_user.profile
     end
 
-    # Only allow a list of trusted parameters through.
     def profile_params
       params.expect(profile: [ :first_name, :last_name, :phone, :avatar, :country, :remove_avatar ])
     end
