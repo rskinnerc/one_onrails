1291f0b36d151ea117e977c9d670454f39ee3112
Ronald Skinner <rjskinnerc@unal.edu.co>
2024-11-23 05:56:52 -0500

    Install Flipper gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_23_101412) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_23_104900) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -68,6 +68,22 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_23_101412) do
     t.datetime "updated_at", null: false
     t.index ["subscription_id"], name: "index_billing_histories_on_subscription_id"
     t.index ["user_id"], name: "index_billing_histories_on_user_id"
+  end
+
+  create_table "flipper_features", force: :cascade do |t|
+    t.string "key", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_flipper_features_on_key", unique: true
+  end
+
+  create_table "flipper_gates", force: :cascade do |t|
+    t.string "feature_key", null: false
+    t.string "key", null: false
+    t.text "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
   end
 
   create_table "plans", force: :cascade do |t|
