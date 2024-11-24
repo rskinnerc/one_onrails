375b7d25d0a0fc83f352c5eb915a4b37f5ee14c4
Ronald Skinner <rjskinnerc@unal.edu.co>
2024-11-24 05:23:09 -0500

    Add flag subscription at sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_23_145739) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_24_100656) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
