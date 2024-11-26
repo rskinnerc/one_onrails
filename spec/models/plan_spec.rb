9ea062bca791105a28589fdce178da7cb8e8ce89
Ronald Skinner <rjskinnerc@unal.edu.co>
2024-11-26 05:00:32 -0500

    Add test to plan specs for initial subscription scope

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Plan, type: :model do
   describe 'associations' do
     it { is_expected.to have_many(:subscriptions) }
   end
+
+  describe 'scopes' do
+    describe '.initial_subscription' do
+      let!(:plan) { create(:plan, initial_subscription: true) }
+
+      it 'returns the plan with initial_subscription set to true' do
+        expect(Plan.initial_subscription).to eq(plan)
+      end
+    end
+  end
 end
