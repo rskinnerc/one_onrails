ae6a970e86b3733f2f758cf4496f9ab5ef7893ab
Ronald Skinner <rjskinnerc@unal.edu.co>
2024-12-12 09:00:24 -0500

    Add address validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :user
 
+  validates :address_line_1, :city, :country, presence: true
   validates :default, uniqueness: { scope: :user_id }, if: :default?
 
   def to_s
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe Address, type: :model do
   describe "validations" do
     subject { build(:address) }
 
+    it { is_expected.to validate_presence_of(:address_line_1) }
+    it { is_expected.to validate_presence_of(:city) }
+    it { is_expected.to validate_presence_of(:country) }
     it { is_expected.to validate_uniqueness_of(:default).scoped_to(:user_id) }
   end
 end
