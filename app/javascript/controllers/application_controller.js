92dd9aa05d8d65d62ebe2334fbfff20d3cd8bb0c
Ronald Skinner <rjskinnerc@unal.edu.co>
2024-12-15 06:42:21 -0500

    Refactor stimulus application controller to handle saving the theme remotely

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -1,25 +1,39 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "themeController", "togglerSwap", "mainDrawerToggle" ]
+  static targets = [ "themeController", "togglerSwap", "mainDrawerToggle", "body" ]
 
   initialize() {
-    let theme = localStorage.getItem('theme')
-    if (theme) {
+    if (this.themeControllerTarget.value === this.bodyTarget.dataset.theme) {
       this.themeControllerTarget.checked = true
     }
   }
 
-  saveTheme(event) {
+  async saveTheme(event) {
     if (event.target.checked) {
-      localStorage.setItem('theme', event.target.value)
+      this.bodyTarget.dataset.theme = 'night'
     } else {
-      localStorage.removeItem('theme')
+      this.bodyTarget.dataset.theme = 'light'
     }
+
+    await fetch('/account/settings', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        "X-CSRF-Token": this.getMetaValue("csrf-token")
+      },
+      body: JSON.stringify({setting: { theme: this.bodyTarget.dataset.theme }})
+    })
   }
 
   toggleSidebar() {
     this.togglerSwapTarget.checked = !this.togglerSwapTarget.checked  
     this.mainDrawerToggleTarget.checked = !this.mainDrawerToggleTarget.checked
+  }
+
+  getMetaValue(name) {
+    const element = document.head.querySelector(`meta[name="${name}"]`)
+    return element.getAttribute("content")
   }
 }
