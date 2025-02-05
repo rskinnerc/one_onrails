7d4391c1ad81685ae1c955b9618350777b81d41c
Ronald Skinner <rjskinnerc@unal.edu.co>
2025-02-05 05:54:38 -0500

    Update tailwind config to look inside the component classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/components/**/*.html.erb',
+    './app/components/**/*.rb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
